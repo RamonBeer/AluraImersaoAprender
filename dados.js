a1eff60fd67957479cb8b8693956fb8e83ee1d57
RamonBeer <rgarciamarinho515@gmail.com>
2024-09-07 12:37:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+{
+    titulo: "Djamila Ribeiro", 
+    descricao: "Djamila Ribeiro é uma filósofa, feminista negra, escritora e ativista brasileira que tem se destacado por suas contribuições para o debate sobre raça, gênero e desigualdade no Brasil.",
+    link: "https://www.fecomercio.com.br/noticia/raca-e-genero-na-sociedade-brasileira-por-djamila-ribeiro-e-amara-moira#:~:text=Ela%20e%20a%20fil%C3%B3sofa%20e,direitos%20das%20minorias%2C%20discrimina%C3%A7%C3%B5es%20de",
+    tags: "filosofa cartas para minha avó"
+},  
+{
+    titulo: "Paulo Freire",
+    descricao: "Paulo Freire foi um educador brasileiro, conhecido internacionalmente por seus trabalhos na área da pedagogia e por desenvolver o método de alfabetização de adultos que leva seu nome.",
+    link: "https://www.paulofreire.org/",
+    tags: "marçal direito educação"
+},
+{
+    titulo: "Sueli Carneiro",
+    descricao: "Filósofa, escritora e ativista antirracista, Sueli Carneiro é uma das principais referências do feminismo negro no Brasil.",
+    link: "https://acervo.casasuelicarneiro.org.br/",
+    tags: "antirracista filosofa ativista"
+}
+];
